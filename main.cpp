c57a82da0710b9070e40e8166dd1154a5259137e
Calvin <Rizkycalvin58@gmail.com>
2024-04-19 23:03:10 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int kuadrat(int x)
+{
+    int y;
+
+    y = x*y;
+    return y;
+}
+
+void tampilkan(int input){
+
+    cout << " menampilkan dengan void\n ";
+    cout << input << endl;
+}
+
+int main(){
+
+    int input,hasil,a,b,hasil2;
+    cout << " nilai kuadrat dari : ";
+    cin >> input;
+}
